a7641c7243e0a4ede1be577b0c655e39d308fbdf
kunzh <kunzh@uio.no>
2021-09-04 17:28:08 +0200

    move function completed.

---
+++ assignment2/movefiles.sh
@@ -0,0 +1,15 @@
+#! /bin/bash
+function movefls () {
+    if [ -d "$1" ] && [ -d "$2" ]; then # -d: FILE exists and is a directory
+        mv "$1"/* "$2"
+        echo "Files from $1 are moved to $2"
+    elif [ -d "$1" ] && [ ! -d "$2" ]; then
+        echo "The destination directory does not exist!"
+    elif [ ! -d "$1" ] && [ -d "$2" ]; then
+        echo "The source directory does not exist!"
+    elif [ ! -d "$1" ] && [ ! -d "$2" ]; then
+        echo "Both the soure and the destination directory do not exist!"
+    fi
+}
+
+movefls "$1" "$2"
