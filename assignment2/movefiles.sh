28e53f38f3d13dd569122cba7f563af68a782877
kunzh <kunzh@uio.no>
2021-09-05 19:16:53 +0100

    ls command added for source folder.

--- assignment2/movefiles.sh
+++ assignment2/movefiles.sh
@@ -1,6 +1,7 @@
 #! /bin/bash
 function movefls () {
     if [ -d "$1" ] && [ -d "$2" ]; then # -d: FILE exists and is a directory
+        ls "./$1"
         mv "$1"/* "$2"
         echo "Files from $1 are moved to $2"
     elif [ -d "$1" ] && [ ! -d "$2" ]; then
