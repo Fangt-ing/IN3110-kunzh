afb2a07a360498f165a4896e8e38d99fa4a7f65c
kunzh <kunzh@uio.no>
2021-09-10 22:40:35 +0100

    move.sh completed.

--- assignment2/move.sh
+++ assignment2/move.sh
@@ -1,17 +1,65 @@
 #!/bin/bash
-function move() {
-    if [ -n "$1" ] && [ -n "$2" ]; then # -n: length of the string is a non-zero.
-        echo "$# directories are indicated."
-        ls "./$1" # list all the files in the src folder.
-        mv "$1"/* "$2" # move all files to the dst folder.
-        echo "Files from $1 are moved to $2."
-    elif [[ -n "$1" && -z "$2" ]]; then # -z: check if the input string length is zero or not.
-        echo "$# directories are indicated."
-        echo "The destination directory does not exist!"
-    elif [ -z "$1" ]; then # regardless of $2, if $1 is empty, then comes the elif
-        echo "$# directories are indicated."
-        echo "Both the soure and the destination directory do not exist!"
-    fi
-}
 
-move $1 $2
+#$1 or number aftert the $ is for being excuted in bash order.
+src=$1
+dst=$2
+filetypes=$3
+
+# move certain types of file works as the following.
+# src=$1, dst=$2, ft=$3
+# mv $src/*.$ft $dst
+
+if [ $# -ge 2 ]; then
+    echo "There are $# arguments."
+    if [ -d $src ]; then
+        if [ -d "$dst" ]; then
+            if [ -n "$filetypes" ]; then
+                echo $filetypes
+                # mv $src/*.$filetyps $dst
+                find $src -name "*.$filetypes" -exec mv "{}" $dst \;
+                echo "All $filetypes files are moved."
+            elif [ -z "$filetypes" ]; then
+                mv $src/* $dst
+                echo "All content in folder $src are moved to folder $dst."
+            fi
+        elif [ ! -d "$dst" ]; then
+            echo "The folder $dst does not exist, do you want to create one?"
+            echo "y/Yes/Y or no/No/n/N"
+            read dirname
+            if [ $dirname = "y" ] || [ $dirname = "Yes" ] || [ $dirname = "Y" ]; then
+                echo "Do you want to append the current time to your directory name?"
+                echo "y/Yes/Y or no/No/n/N"
+                read appendtime
+                if [ $appendtime = "y" ] || [ $appendtime = "Yes" ] || [ $appendtime = "Y" ]; then
+                    mkdir $dst-$(date +"%Y-%h-%d_%H:%M")
+                    echo "The destination folder $dst-$(date +"%Y-%h-%d_%H:%M") is created."
+                elif [ $appendtime = "no" ] || [ $appendtime = "No" ] || [ $appendtime = "n" ] || [ $appendtime = "N" ]; then
+                    mkdir $dst
+                    echo "The destination folder $dst is created."
+                fi
+            elif [ $dirname = "no" ] || [ $dirname = "No" ] || [ $dirname = "n" ] || [ $dirname = "N" ]; then
+                echo "The destination folder $dst is not created."
+            else
+                echo "You did not enter a correct answer, the operation is aborted."
+            fi
+        fi
+    else
+        echo "The source folder does not exist."
+        echo "The operation is aborted."
+    fi
+
+# the following block describe the situations when arguments are less than 2. ie. 0 and 1.
+elif [ 2 -gt $# ]; then
+    if [ -d "$src" ]; then
+        echo "The source folder $src exist, but your argument input is only $#."
+        echo "There has to be at least 2 arguments."
+    elif [ ! -d $src ]; then
+        echo "The source folder $src does not exist."
+    fi
+
+    echo "You argument input is only $#."
+    echo "There has to be at least 2 arguments."
+elif [ -z $src ]; then
+    echo "You argument input is only $#."
+    echo "There has to be at least 2 arguments."
+fi
