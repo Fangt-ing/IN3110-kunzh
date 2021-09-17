72da7f3964569e29ac7b89eff376a9acb6d082aa
Kun Zhu <kunzh@ulrik.uio.no>
2021-09-17 17:21:03 +0200

    is_equal function completed.

--- assignment3/array.py
+++ assignment3/array.py
@@ -1,6 +1,9 @@
 from ast import If
+from msilib.schema import SelfReg
 from pydoc import Doc
+from time import sleep
 from turtle import shape
+from xmlrpc.client import boolean
 from matplotlib import docstring
 
 
@@ -26,18 +29,44 @@ class Array:
         # Check if the values are of valid type
 
         # Optional: If not all values are of same type, all are converted to floats.
-        self.values = []
-        self.shape = ()
-        pass
-    def __shapevalue__(self):
-        """Check the array shape to match the input values. And the array input type.
+        self.elements = list(values)
+        # self.demention = tuple(shape)    
+        self.__str__()    
+
+    def __valuetype__(self):
+        """Checks the array input values, and the array input type.
         
         Returns:
-            True: if both the array shape match value amount and array values are in the same data type.
+            True: if both the array values are in the same data type.
             False: if not True.
         """
+        if type(self.elements) == int or type(self.elements) == float or\
+        type(self.elements) == boolean:
+            for i in range(len(self.elements)):
+                if i < len(self.elements) - 1:
+                    # Check the if all elements' type matches
+                    return type(self.elements[i]) == type(self.elements[i + 1])
+        else:
+            print(
+                'Array values must have the same data type of either int, float or bolean.'
+            )
+            return False
+
+    def __shapematch__(self):
+        """Checks the demention of the array with the amount of values.
         
-        
+        Returns:
+            True: if demention values matches the amount of values.
+            False: if not True.
+        """
+        if len(self.demention) == 1:
+            if self.demention[0] == len(self.elements):
+                return True
+        else:
+            print(
+                "THe array can be only 1D array, please assing a correct value."
+            )
+
     def __str__(self):
         """Returns a nicely printable string representation of the array.
 
@@ -47,8 +76,7 @@ class Array:
         """
         # for i in range(len(self.vlaues)):
         #     self.values.append(self.values[i])
-        print(str(list(self.values)))
-
+        return str(self.elements)
 
     def __add__(self, other):
         """Element-wise adds Array with another Array or number.
@@ -66,7 +94,6 @@ class Array:
 
         # check that the method supports the given arguments (check for data type and shape of array)
 
-        pass
 
     def __radd__(self, other):
         """Element-wise adds Array with another Array or number.
@@ -81,7 +108,14 @@ class Array:
             Array: the sum as a new array.
 
         """
-        pass
+        # shapematch is not needed for this.
+        if self.__valuetype__():
+            for i in range(len(self.elements)):
+                self.elements[i] = self.elements[i] + other
+            return self.elements
+        elif not self.__shapematch__() or not self.__valuetype__():
+            return "NotImplemented!"
+        
 
     def __sub__(self, other):
         """Element-wise subtracts an Array or number from this Array.
@@ -111,7 +145,13 @@ class Array:
             Array: the difference as a new array.
 
         """
-        pass
+        # shapematch is not need.
+        if self.__valuetype__():
+            for i in range(len(self.elements)):
+                self.elements[i] = self.elements[i] - other
+            return self.elements
+        elif not self.__shapematch__() or not self.__valuetype__():
+            return "NotImplemented!"
 
     def __mul__(self, other):
         """Element-wise multiplies this Array with a number or array.
@@ -144,6 +184,12 @@ class Array:
         # Hint: this solution/logic applies for all r-methods
         return self.__mul__(other)
 
+    def __elementscheck__(self, other):
+        '''Returns the elements in the given array.
+        '''
+        for i in range(len(self.elements)):
+            return self.elements[i] == other[i]
+        
     def __eq__(self, other):
         """Compares an Array with another Array.
 
@@ -157,7 +203,23 @@ class Array:
             bool: True if the two arrays are equal (identical). False otherwise.
 
         """
-        pass
+        if type(other) != list:
+            return False
+        
+        elif type(other) == list:
+            if len(self.elements) != len(len(other)):
+                return False
+            elif len(self.elements) == len(len(other)):
+                for i in range(self.elements):
+                    if self.elements[i] == other[i]:
+                        eq = True
+                    elif self.elements[i] == other[i]:
+                        eq = False
+                        break
+                return eq
+            elif type(other) != int or float or bool:
+                return False
+            
 
     def is_equal(self, other):
         """Compares an Array element-wise with another Array or number.
@@ -177,8 +239,25 @@ class Array:
             ValueError: if the shape of self and other are not equal.
 
         """
-
-        pass
+        if type(other) != list:
+            if type(other) == int or float or bool:
+                pass
+            else:
+                raise TypeError('The values of the array is not correct, \
+                    it should be at either an in or float or boolean type.')
+        equal = []
+        if type(other) == list:
+            if len(self.elements) != len(other):
+                raise ValueError('Shapes of the 2 arrays do not match.')
+            elif len(self.elements) == len(other):
+                for i in range(len(self.elements)):
+                    if self.elements[i] == other[i]:
+                        equal.append(True)
+                    elif not self.elements[i] == other[i]:
+                        equal.append(False)
+                return equal
+            else:
+                print('Length of the array do not match!')
 
     def min_element(self):
         """Returns the smallest value of the array.
